4bd1fa40d97556b63ec1d17e443ceb3e1eaa36dd
Hung Nguyen <hung24021994@gmail.com>
2020-09-01 16:46:47 +0700

    Update docker

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+  res.send('<h1>Hello World23232</h1>');
 });
 
 app.listen(PORT, HOST);
